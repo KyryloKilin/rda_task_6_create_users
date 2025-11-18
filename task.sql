97a571850dd7702cb85163f2b7eec1f3032d4138
root <root@vmi2772850.contaboserver.net>
2025-11-18 19:03:11 +0000

    Solution

--- task.sql
+++ task.sql
@@ -1,1 +1,8 @@
--- write your code for database user creation here
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'p@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON ShopDB.* TO 'webappuser'@'%';
+
+
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'p@ssw0rd';
+GRANT ALL PRIVILEGES
+ON ShopDB.* TO 'deploymentuser'@'%';
